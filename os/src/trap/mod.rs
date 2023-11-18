8e3765fe27fa10c76b76e6c1dc7c3ed0f82fac39
lllzzzcccc <21301044@bjtu.edu.cn>
2023-11-18 15:24:15 +0000

    New os/src/trap/mod.rs by ZhiChao Li in 21301044

---
+++ os/src/trap/mod.rs
@@ -0,0 +1,11 @@
+use core::arch::global_asm;
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+    extern "C" { fn __alltraps(); }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
+
