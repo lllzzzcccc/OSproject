58e876f5e10b0f193e02631a823c70bdd520137e
lllzzzcccc <21301044@bjtu.edu.cn>
2023-12-01 03:52:38 +0000

    Compile project by ZhiChao Li in 21301044

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,4 +1,4 @@
-iconst SYSCALL_WRITE: usize = 64;
+const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
 
@@ -10,10 +10,10 @@ use process::*;
 
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
+	SYSCALL_YIELD => sys_yield(),
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
-	SYSCALL_YIELD => sys_yield(),
     }
 }
 
