2d70c76040288f63f113c58e0e6d748d4470adc0
lllzzzcccc <21301044@bjtu.edu.cn>
2023-12-04 04:06:38 +0000

    Complete preemptive scheduling by ZhiChao Li in 21301044

---
+++ os/src/timer.rs
@@ -0,0 +1,21 @@
+use riscv::register::time;
+use crate::sbi::set_timer;
+use crate::config::CLOCK_FREQ;
+
+const MSEC_PER_SEC: usize = 1000;
+const TICKS_PER_SEC: usize = 100;
+
+pub fn set_next_trigger() {
+    set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
+}
+
+
+pub fn get_time() -> usize {
+    time::read()
+}
+
+pub fn get_time_ms() -> usize {
+    time::read() / (CLOCK_FREQ / MSEC_PER_SEC)
+}
+
+
