4eaf59c222a0aab04f7e1282495dc55c26a6496e
lllzzzcccc <21301044@bjtu.edu.cn>
2023-12-15 03:13:11 +0000

    Define page table item data structure by ZhiChao Li in 21301044

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,5 +1,11 @@
 mod heap_allocator;
 
+mod page_table;
+
+use page_table::{PTEFlags};
+pub use page_table::{PageTableEntry};
+
+
 pub fn init() {
     heap_allocator::init_heap();
     heap_allocator::heap_test();
