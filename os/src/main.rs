be846340348b29b0c697ebb8e5580ffcc210bda5
lllzzzcccc <21301044@bjtu.edu.cn>
2023-12-15 04:46:49 +0000

    Update os by ZhiChao Li in 21301044

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,6 +5,11 @@
 
 use core::arch::global_asm;
 
+extern crate alloc;
+
+#[macro_use]
+extern crate bitflags;
+
 #[macro_use]
 mod console;
 mod lang_items;
@@ -18,11 +23,6 @@ mod timer;
 mod sync;
 mod mm;
 
-#[macro_use]
-extern crate bitflags;
-
-extern crate alloc;
-
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
@@ -31,9 +31,12 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| {
-        unsafe { (a as *mut u8).write_volatile(0) }
-    });
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as usize as *mut u8,
+            ebss as usize - sbss as usize,
+        ).fill(0);
+    }
 }
 
 #[no_mangle]
@@ -41,11 +44,12 @@ pub fn rust_main() -> ! {
     clear_bss();
     println!("[kernel] Hello, world!");
     mm::init();
+    println!("[kernel] back to world!");
+    mm::remap_test();
     trap::init();
-    loader::load_apps();
-    task::run_first_task();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
+    task::run_first_task();
     panic!("Unreachable in rust_main!");
 }
 
